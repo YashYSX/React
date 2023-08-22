a473d77c3c302d2a6037cc362a97e8ab0e35bf02
unknown <yashsaini.saini35@gmail.com>
2023-08-22 17:13:06 +0530

    imagecard

---
+++ day1_myapp/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    (<h1>hello</h1>),
+    (<p>yashysx</p>),
+    (<h2>ysx</h2>)
+    
+    
+  );
+}
+
+export default App;
---
+++ day1_myapp/src/data.js
@@ -0,0 +1,15 @@
+const data = [
+    {
+        title:"car",
+        desc: "vrooommmmmmmmmmmmmmmmmmmmmmmmmm",
+        Image:("https://upload.wikimedia.org/wikipedia/commons/1/16/Bugatti_Divo%2C_GIMS_2019%2C_Le_Grand-Saconnex_%28GIMS0029%29.jpg"),
+
+    },
+
+    {
+        title :"car",
+        desc : "vrooommmmmmmmmmmmmmmmmmmmmmmmmm",
+        Image:("https://upload.wikimedia.org/wikipedia/commons/1/16/Bugatti_Divo%2C_GIMS_2019%2C_Le_Grand-Saconnex_%28GIMS0029%29.jpg"),
+
+    }
+]
