632ec26d9b1ed9ee53e06160feb31e5ecb697f26
unknown <yashsaini.saini35@gmail.com>
2023-08-25 17:10:14 +0530

    some changes

--- testapp/src/component/ImageCard.jsx
+++ testapp/src/component/ImageCard.jsx
@@ -1,6 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+
+const handleClick = ()=>{
+  const  [count,setCount] = useState(10);
+  console.log("button click");
+};
 
 const ImageCard = (props)=> {
   return (
@@ -8,11 +13,12 @@ const ImageCard = (props)=> {
         <Card style={{ width: '18rem' }}>
       <Card.Img variant="top" src={props.imgurl} />
       <Card.Body>
+        {props.id}
         <Card.Title>{ props.title}</Card.Title>
         <Card.Text>
         {props.desc}
         </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
+        <Button onClick={handleClick} variant="primary">Go somewhere</Button>
       </Card.Body>
     </Card>
 
